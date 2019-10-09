bebcccd45ab625018ed424b10783096abaf43c96
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-09 17:37:55 +0900

    Product

---
+++ Product.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method = "post" action = "doProduct.php">
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type = "text" name= "txtName"></td>
+			
+			<tr>
+				<td>Price</td>
+				<td><input type = "number" name = "price"/></td>
+			</tr>
+			
+			<tr>
+				<td></td>
+				<td><input type = "submit" name = "Submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>

293229af3b6ed755f7d13482cab499523d396142
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-23 16:04:25 +0900

    assignment2

---
+++ assignment2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignment2</title>
+	<link rel = "asm2style" href = "./asm2style.css">
+</head>
+<body>
+	<?php
+	require_once("./DBconnect(asm).php");
+	?>
+	
+
+	<?php
+	require_once("./first(asm).php");
+	?>
+
+	
+
+		<div class="second">
+			<img src="./assignment2 images/home.png" alt = "shop" width = "100%" height = "100%">
+			
+		</div>
+
+	<?php
+	require_once("./third(asm).php");
+	?>	
+
+</body>
+</html>

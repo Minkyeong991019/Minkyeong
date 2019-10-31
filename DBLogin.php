a3e5b662f8d844a5f062bfddde75da0200342e8b
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-31 16:56:48 +0900

    xxx

--- DBLogin.php
+++ DBLogin.php
@@ -6,36 +6,34 @@
 	<link rel="stylesheet" href="./stylesheethome.css">
 </head>
 <body  style="background-color: #FDC4C4">
-<style>
-		
- </style>
-<table cellspacing="0" cellpadding="0" width=100% height=auto>
-<tr>
-	<td><img src="./Toy2.jpg" style="width: 100px; height: 100px;" style = "display: inline;" /></td>
-	<td><b  style = "color: #FEFBFB; font-size: 50px;" class="logo">ATN M Toys</b></td>
-	<td><input type="text"  placeholder="Search..." name="search" class="search-box"></td>
-	<td><b><a href="Login.php">LOGIN</a></b></td>
-	<td><img src="https://img.icons8.com/ios-glyphs/30/000000/add-shopping-cart.png"></td>
-</tr>
-</table>
 
-<table cellspacing="10" cellpadding="0" width=100% height=auto>
-		<tr>
-			<td><div class="header">
-			<ul>
-				
-				<li><b><a href="Assignment2(home).php">HOME</a></b></li>
-				<li><b><a href="Allproduct.php">SHOP</a></b></li>
-			</ul>
-			</div>
-			</td>
-		</tr>
-</table>
-<table style="margin-left: 37%;" height="300" >
-        	<tr>
-				<td align="center" colspan="2"><h1>LOGIN</h1></td>
+	<table cellspacing="0" cellpadding="0" width=100% height=auto>
+	<tr>
+		<td><img src="./Toy2.jpg" style="width: 100px; height: 100px;" style = "display: inline;" /></td>
+		<td><b  style = "color: #FEFBFB; font-size: 50px;" class="logo">ATN M Toys</b></td>
+		<td><input type="text"  placeholder="Search..." name="search" class="search-box"></td>
+		<td><b><a href="Login.php">LOGIN</a></b></td>
+		<td><img src="https://img.icons8.com/ios-glyphs/30/000000/add-shopping-cart.png"></td>
+	</tr>
+	</table>
 
+	<table cellspacing="10" cellpadding="0" width=100% height=auto>
+			<tr>
+				<td><div class="header">
+				<ul>
+					
+					<li><b><a href="Assignment2(home).php">HOME</a></b></li>
+					<li><b><a href="Allproduct.php">SHOP</a></b></li>
+				</ul>
+				</div>
+				</td>
 			</tr>
+	</table>
+	<table style="margin-left: 37%;" height="300" >
+        	<tr>
+				<td align="center" colspan="2"><h1>PRODUCT</h1></td>
+			</tr>
+	</table>
 
 
 

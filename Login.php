6d93120fbff055bdd5e6bc7baac1179eddfe96f1
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-31 13:08:48 +0900

    fixlogin

--- Login.php
+++ Login.php
@@ -56,42 +56,7 @@
 	</div>
 		</div>
 		
-		<?php
-
-		$error = "";
-
-		$conn = pg_connect("host=localhost dbname=brittains_db user=brittains password=XXXXXX" );
-
-		$sql = "SELECT * FROM acount";
-		$result = pg_query($conn, $sql);
-
-		if($_SERVER["REQUEST_METHOD"] == "GET") {
-		    $userName="";
-		    $password="";
-		}
-
-		else if($_SERVER["REQUEST_METHOD"] == "POST") {
-
-		    $userName=trim($_POST["userNameLogin"]);
-		    $password=trim($_POST["passwordLogin"]);
-
-		    if(pg_fetch_result($results, $userName, "userName")==true 
-		       && pg_fetch_result($results, $password, "userName")==true) {
-		        setcookie("userIDforDV", $userName, time()+43200);
-		    }
-		    else {
-		        $error = "Your username and or password is incorrect";
-		    }
-
-		}
-
-		$userName = $_COOKIE['userIDforDV'];
-
-		if(isset($userName) && $userName!="") {
-		    echo "Welcome " . $userName;
-		}
-
-		echo $error;
+		
 
 </body>
 </html>

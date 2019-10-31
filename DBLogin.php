ba5a0150ca3506a7397834c9d48d96b52289e76b
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-11-01 01:57:00 +0900

    Update DBLogin.php

--- DBLogin.php
+++ DBLogin.php
@@ -16,29 +16,29 @@
     	<br>
 
 	<table cellspacing="0" cellpadding="0" width=100% height=auto>
-<tr>
-	<td><img src="./Toy2.jpg" style="width: 100px; height: 100px;" style = "display: inline;" /></td>
-	<td><b  style = "color: #FEFBFB; font-size: 50px;" class="logo">ATN M Toys</b></td>
-	<td><input type="text"  placeholder="Search..." name="search" class="search-box"></td>
-	<td><b><a href="Login.php">LOGIN</a></b></td>
-	<td><img src="https://img.icons8.com/ios-glyphs/30/000000/add-shopping-cart.png"></td>
-</tr>
-</table>
-
-<table cellspacing="10" cellpadding="0" width=100% height=auto>
 		<tr>
-			<td><div class="header">
-			<ul>
-				
-				<li><b><a href="Assignment2(home).php">HOME</a></b></li>
-				
-				<li><b><a href="Allproduct.php">SHOP</a></b></li>
-				
-			</ul>
-			</div>
-			</td>
+			<td><img src="./Toy2.jpg" style="width: 100px; height: 100px;" style = "display: inline;" /></td>
+			<td><b  style = "color: #FEFBFB; font-size: 50px;" class="logo">ATN M Toys</b></td>
+			<td><input type="text"  placeholder="Search..." name="search" class="search-box"></td>
+			<td><b><a href="Login.php">LOGIN</a></b></td>
+			<td><img src="https://img.icons8.com/ios-glyphs/30/000000/add-shopping-cart.png"></td>
 		</tr>
-</table>
+	</table>
+
+	<table cellspacing="10" cellpadding="0" width=100% height=auto>
+			<tr>
+				<td><div class="header">
+				<ul>
+					
+					<li><b><a href="Assignment2(home).php">HOME</a></b></li>
+					
+					<li><b><a href="Allproduct.php">SHOP</a></b></li>
+					
+				</ul>
+				</div>
+				</td>
+			</tr>
+	</table>
 
 	<br> 
     	<?php

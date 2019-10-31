0cd7c9d41a69cd087f5d44ddac4ad7bbe7bbf1e8
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-31 16:49:39 +0900

    Update Allproduct.php

--- Allproduct.php
+++ Allproduct.php
@@ -31,23 +31,35 @@
 		</tr>
 </table>
 
-			<?php
-				$sql = "SELECT * toyid, image, price FROM toyproduct";
-				$stmt = $pdo->prepare($sql);
-				//execute the query on the server and return the result set
-				$stmt->setFetchMode(PDO::FETCH_ASSOC);
-				$stmt->execute();
-				$resultSet = $stmt->fetchAll();
-				//display the data 
+			
+			<?php 
+				$name = $_POST["txtName"];
+				$price = $_POST["txtprice"];
+				
+				
+				//Refere to database 
+			   $db = parse_url(getenv("DATABASE_URL"));
+			   $pdo = new PDO("pgsql:" . sprintf(
+			        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+			        $db["host"],
+			        $db["port"],
+			        $db["user"],
+			        $db["pass"],
+			        ltrim($db["path"], "/")
+			   ));
+			   $data = [
+				    'toyname' => $name,
+				    'price' => $price
+				];
+				$stmt =  $pdo->prepare("INSERT INTO product(toyname, price) VALUES (:toyname,:price)");	
+				$stmt->execute($data);
 			?>
-			<ul>
-				<?php
-					foreach ($resultSet as $row) {
-						echo "<li>" .
-							$row["toyid"] . '--'. $row["toyname"] . '--'. $row["image"]. '--'. $row["price"]. '--'. $row["catid"]
-						. "</li>";
-					}
-				?>
-			</ul>
+			 <h2>Thank you <?php echo $name?>  for submit Product 
+
+			 </h2>
+			 <ul>
+			 	<li><?php echo $price?></li>
+			 </ul>
+			 <a href="DBLogin.php">Login</a>
 	</body>
 </html>

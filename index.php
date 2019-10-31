51f5dd3d285924a7bae3cb46b68690940c2e0c01
Minkyeong991019 <55874219+Minkyeong991019@users.noreply.github.com>
2019-10-31 11:16:10 +0900

    indexfix

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-<!DOCTYPE html>
 <html>
+<head>
+    <!-- Required meta tags -->
+   
+    <title>Product Database</title>
+  </head>
 <body>
 
-<h1>My first PHP page</h1>
+<h1>Register Page</h1>
 
 <?php
-echo "Show all rows from Postgres Database";
+	echo "Showing all rows from Postgres Database";
+	
+	//Refere to database 
+	$db = parse_url(getenv("DATABASE_URL"));
 
-//Refere to database
-$db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
     $db["host"],
@@ -17,62 +22,24 @@ $pdo = new PDO("pgsql:" . sprintf(
     $db["pass"],
     ltrim($db["path"], "/")
 ));
-	//you sql query
-
-	$sql = "SELECT  username, password FROM account";
-	$sql = "SELECT  catname, img FROM category";
-	$sql = "SELECT  productname, price FROM product";
-	$sql = "SELECT  toyname, image, price FROM toyproduct";
-
+	//your sql query
+	$sql = "SELECT * FROM registercourse";
 	$stmt = $pdo->prepare($sql);
 	//execute the query on the server and return the result set
 	$stmt->setFetchMode(PDO::FETCH_ASSOC);
 	$stmt->execute();
 	$resultSet = $stmt->fetchAll();
-	//desplay the data
-?> 
-
-			
-	<ul>
-		<?php
-			foreach ($resultSet as $row)
-			{
-				echo"<li>" .
-					$row["username"] . '--'. $row["password"]. "</li>";
-			}
-		?>
-	</ul>
-
-	<ul>
-		<?php
-			foreach ($resultSet as $row)
-			{
-				echo"<li>" .
-					$row["catname"] . '--'. $row["price"]. "</li>";
-			}
-		?>
-	</ul>
-
-	<ul>
-		<?php
-			foreach ($resultSet as $row)
-			{
-				echo"<li>" .
-					$row["productname"] . '--'. $row["price"]. "</li>";
-			}
-		?>
-	</ul>
-
-	<ul>
-		<?php
-			foreach ($resultSet as $row)
-			{
-				echo"<li>" .
-					$row["toyname"] . '--'. $row["image"]. '--' . $row["price"] "</li>";
-
-			}
-		?>
-	</ul>
+	//display the data 
+?>
+<ul>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" .
+				$row["studentname"] . '--'. $row["course"] . '--'. $row["dob"] . '--'. $row["gender"] . '--'. $row["fav"]
+			. "</li>";
+		}
+	?>
+</ul>
 
 </body>
 </html>

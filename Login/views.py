3b06d925dd52f405e1470cf16191ab5f525beae1
buidinhkha <57929886+buidinhkha@users.noreply.github.com>
2021-04-20 03:02:31 +0700

    funtion login

--- Login/views.py
+++ Login/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from .models import User
+from django.db import connection
+from django.shortcuts import redirect
 
 
 # Create your views here.
@@ -14,23 +18,20 @@ def loginManager(request):
     if request.method == 'POST':
         userName = request.POST.get('userName','')
         passWord = request.POST.get('passWord','')
-        print(userName)
-        print(passWord)
         user = authenticate(username=userName, password=passWord)
         if(user is not None):
             userID =  User.objects.filter(username = userName)[0].id
+            print(userID)
             with connection.cursor() as cursor:
                 cursor.execute(
                     "SELECT auth_group.name FROM auth_group INNER JOIN login_user_groups ON auth_group.id = login_user_groups.group_id INNER JOIN login_user ON login_user.id = login_user_groups.user_id WHERE login_user.id = '%s'" ,
                 [userID]
                 )
-            auth_group = cursor.fetchall()[0][0]
-            request.session.set_expiry(86400)
-            auth_login(request, user)
+                auth_group = cursor.fetchall()[0][0]
             if(auth_group == "Manager"):
                 return redirect('/Manager/')
-            elif(auth_group == "Gust"):
-                return redirect('/Gust/')
+            elif(auth_group == "Guest"):
+                return redirect('/')
         else:
             error = {'error': 'Username or password is incorrect !'}
             return render(request, 'login.html', error)
